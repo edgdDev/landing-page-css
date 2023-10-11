d4778f63280a2673230cbe8a8c86c7986c9f0833
Brandon <fernandez.b@dhn.com.do>
2023-10-11 16:46:50 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+const menu = document.querySelector('.nav__menu');
+const menuList = document.querySelector('.nav__list');
+const links = document.querySelectorAll('.nav__link');
+
+menu.addEventListener('click', () => menuList.classList.toggle('nav__list--show'));
+
+links.forEach( link => {
+    link.addEventListener('click', () => menuList.classList.remove('nav__list--show'));
+} )
